a6142f6178f9c02d54ced4a9279f4bb31b57e35f
shlomi cohen <100487390+shlomi888c@users.noreply.github.com>
2023-06-15 23:37:00 +0300

    Add files via upload

---
+++ chef_file/client.rb
@@ -0,0 +1,4 @@
+chef_server_url          "https://manage.chef.io/organizations/shlomitest"
+validation_key           "/etc/chef/shlomitest-validator.pem"
+validation_client_name   "shlomitest-validator"
+node_name                "shlomitest1"
